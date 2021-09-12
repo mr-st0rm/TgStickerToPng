e530870f53c505c8e19ffb47288e6fc6cca91fa8
Storm <48455163+St0rm1k@users.noreply.github.com>
2021-09-12 16:07:45 +0500

    Update stickers.py

--- handlers/stickers.py
+++ handlers/stickers.py
@@ -8,7 +8,7 @@ from converter import convert
 @dp.message_handler(content_types=types.ContentTypes.STICKER)
 async def stickers(message: types.Message):
     downloaded_file = await message.sticker.download()
-    downloaded_file.close()
+    downloaded_file.close() # close downloaded file object
 
     send_file = convert(downloaded_file.name.split("/")[1].split('.')[0])  # name of sending file
 
